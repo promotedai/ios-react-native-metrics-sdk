1512b16d99541ee42fe16658be87e6afbb1247eb
Yu-Hong Wang <limepi@gmail.com>
2021-03-12 17:23:34 -0800

    wip

--- Sources/PromotedReactNativeMetricsSDK/PromotedLoggerModule.m
+++ Sources/PromotedReactNativeMetricsSDK/PromotedLoggerModule.m
@@ -1,6 +1,6 @@
 #import "React/RCTBridgeModule.h"
 
-@interface RCT_EXTERN_REMAP_MODULE(PromotedLoggerModule, PromotedLoggerModule, NSObject)
+@interface RCT_EXTERN_MODULE(PromotedLoggerModule, NSObject)
 
 RCT_EXTERN_METHOD(startSessionAndLogUserWithID:(NSString *)userID)
 
