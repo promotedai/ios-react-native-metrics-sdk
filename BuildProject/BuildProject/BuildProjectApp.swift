02e91840ce9c720c824ef9844fc94fb3112f26c5
Yu-Hong Wang <limepi@gmail.com>
2021-03-12 18:01:23 -0800

    Add project for building.

---
+++ BuildProject/BuildProject/BuildProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuildProjectApp.swift
+//  BuildProject
+//
+//  Created by Yu-Hong Wang on 3/12/21.
+//
+
+import SwiftUI
+
+@main
+struct BuildProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BuildProject/BuildProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BuildProject
+//
+//  Created by Yu-Hong Wang on 3/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
