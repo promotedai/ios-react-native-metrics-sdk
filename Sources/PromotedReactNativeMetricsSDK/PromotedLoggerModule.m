fde03eec02f736568628d7d694e56b53e7ba1318
Yu-Hong Wang <limepi@gmail.com>
2021-03-16 18:22:28 -0700

    Implement simple collection view impression logging.

--- Sources/PromotedReactNativeMetricsSDK/PromotedLoggerModule.m
+++ Sources/PromotedReactNativeMetricsSDK/PromotedLoggerModule.m
@@ -8,30 +8,39 @@ RCT_EXTERN_METHOD(startSessionAndLogUserWithID:(NSString *)userID)
 RCT_EXTERN_METHOD(startSessionAndLogSignedOutUser)
 
 #pragma mark - Impressions
+// content is NSDictionary<NSString, id>
 RCT_EXTERN_METHOD(logImpressionWithContent:(nullable NSDictionary *)content)
 
 #pragma mark - Clicks
+// content is NSDictionary<NSString, id>
 RCT_EXTERN_METHOD(logClickToLikeContent:(nullable NSDictionary *)content didLike:(BOOL)didLike)
 
 RCT_EXTERN_METHOD(logClickToShowScreenName:(NSString *)screenName)
 
+// content is NSDictionary<NSString, id>
 RCT_EXTERN_METHOD(logClickToShowScreenName:(NSString *)screenName forContent:(nullable NSDictionary *)content)
 
 RCT_EXTERN_METHOD(logClickToSignUpWithUserID:(NSString *)userID)
 
+// item is NSDictionary<NSString, id>
 RCT_EXTERN_METHOD(logClickToPurchaseItem:(nullable NSDictionary *)item)
 
 RCT_EXTERN_METHOD(logClickWithActionName:(NSString *)action)
 
+// content is NSDictionary<NSString, id>
 RCT_EXTERN_METHOD(logClickWithActionName:(NSString *)action content:(nullable NSDictionary *)properties)
 
 #pragma mark - Views
 RCT_EXTERN_METHOD(logViewWithScreenName:(NSString *)screenName)
 
 #pragma mark - ImpressionLogger
-RCT_EXTERN_METHOD(collectionViewDidLoadWithItems:(NSArray<NSDictionary *> *)items collectionViewName:(NSString *)collectionViewName)
+// items is NSArray<NSArray<NSDictionary<String, id>>>
+RCT_EXTERN_METHOD(collectionViewDidLoad:(NSArray *)items collectionViewName:(NSString *)collectionViewName)
 
-RCT_EXTERN_METHOD(collectionViewDidChangeWithVisibleRows:(NSArray<NSNumber *> *)visibleRows collectionViewName:(NSString *)collectionViewName)
+// visibleIndexes is NSArray<NSArray<NSNumber>>
+RCT_EXTERN_METHOD(collectionViewDidChange:(NSArray *)visibleIndexes collectionViewName:(NSString *)collectionViewName)
+
+RCT_EXTERN_METHOD(collectionViewDidUnmount:(NSString *)name)
 
 #pragma mark - ScrollTracker
 
